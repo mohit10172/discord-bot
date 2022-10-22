74bce62b1df99682956c461630ee5a515dd1fbb4
Mohit <mohit.dhere1106@gmail.com>
2022-10-22 19:35:24 +0530

    event handler added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 //Create a Client instance.
@@ -10,48 +10,21 @@ const client = new Client({
     ], 
 });
 
-//Create an object for storing all the commands.
-client.commands = new Collection();
 
-//Get the path and files related to commands.
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+//Get the path and files related to events.
+const eventsPath = path.join(__dirname, 'event');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-//Store all the commands in the 'client.commands' Collection.
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
 
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
     } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        console.on(event.name, (...args) => event.execute(...args));
     }
 }
-
-//Reay the bot to log on the Guild.
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-//Create a listener to capture any slash commands from the users.
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
 
 //Login  using the bot token.
 client.login(token);
