0a725e450041507fef77723d06c557f4268fb504
Mohit <mohit.dhere1106@gmail.com>
2022-10-23 15:24:44 +0530

    changed the file structure and added echo

---
+++ handlers/commandHandler.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+	const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+	
+	for (const file of commandFiles) {
+		const command = require(`../commands/${file}`);
+		const filePath = `../commands/${file}`;
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+};
